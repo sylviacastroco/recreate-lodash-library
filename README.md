6f2c78e25179b8f89ea056b31ebd813db1d27656
sylvia <hello@sylviacastro.co>
2019-11-23 16:44:42 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Re-create-Lodash-Library
+
+### _Capstone project for Programming in JavaScript_
+
+In this project I implemented _ten methods_ from the incredibly popular Lodash JavaScript library:
+
+- clamp()
+- inRange()
+- words()
+- pad()
+- has()
+- invert()
+- findKey()
+- drop()
+- dropWhile()
+- chunk()
---
+++ _.js
@@ -0,0 +1,41 @@
+const _ = {
+  clamp(number, lower, upper) {
+    lowerClampedValue = Math.max(number, lower);
+    clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
+  inRange(number, start, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    const isInRange = start <= number && number < end;
+    return isInRange;
+  },
+  words(string) {
+    let words = string.split(' ');
+    return words;
+  },
+  pad(string, length) {
+    if (length <= string.length) {
+      return string;
+    }
+    const startPaddingLength = Math.floor((length - string.length) / 2);
+    const endPaddingLength = length - string.length - startPaddingLength;
+    const paddedString =
+      ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
+    return paddedString;
+  },
+  has(object, key) {
+    const hasValue = object[key] != undefined ? true : false;
+    return hasValue;
+  }
+};
+
+// Do not write or modify code below this line.
+module.exports = _;
